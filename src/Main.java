a01c9008f8be72b9e63c6d0de650e1afc003a6e0
TarasevichVlad24 <gagagdet@gmail.com>
2022-11-23 21:52:10 +0200

    Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner s = new Scanner(System.in);
+        System.out.println("Введите имя");
+        String string = s.nextLine();
+        System.out.println("Hello "+string);
 
-        System.out.println("Hello world!");
+
+
+
+
+
     }
 }
